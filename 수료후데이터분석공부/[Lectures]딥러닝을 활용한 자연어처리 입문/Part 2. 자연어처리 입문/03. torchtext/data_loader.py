9cacf8abefefdd1bc304db0cc4f83cc77c958f0a
SEONYEONG YUN <seonyeongyun@naver.com>
2021-08-12 16:34:30 +0900

    [21.08.12] Deep Learning Study - TorchText

---
+++ 수료후데이터분석공부/[Lectures]딥러닝을 활용한 자연어처리 입문/Part 2. 자연어처리 입문/03. torchtext/data_loader.py
@@ -0,0 +1,25 @@
+from torchtext import data
+
+# To load text file using torchtext library.
+class DataLoader(object):
+
+    def __init__(self, train_fn, batch_size=64, valid_ratio=.2, device=-1, 
+                max_vocab=999999, min_freq=1, use_eos=False, shuffle=True):
+        super().__init__()
+
+        # Define field of the input file.
+        self.label = data.Field(sequential=False, use_vocab=True, unk_token=None)
+        self.text = data.Field(use_vocab=True, batch_first=True, include_lengths=False, eos_token='<EOS>' if use_eos else None)
+
+        # Separate
+        train, valid = data.TabularDataset(path=train_fn, format='tsv', 
+                                           fields=[('label', self.label), ('text', self.text)],).split(split_ratio=(1-valid_ratio))
+        
+        # Make iterator.
+        # Sort by length to group similar lengths.
+        self.train_loader, self.valid_loader = data.BucketIterator.splits((train, valid), batch_size=batch_size,
+                                                                        device='cuda:%d' % device if device >= 0 else 'cpu', shuffle=shuffle,
+                                                                        sort_key=lambda x: len(x.text), sort_within_batch=True)
+        # Make vocabulary for label and text.
+        self.label.build_vocab(train)
+        self.text.build_vocab(train, max_size=max_vocab, min_freq=min_freq)                                                                
